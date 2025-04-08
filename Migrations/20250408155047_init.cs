281e5ab110a9937859fb6385a954694ebdd4ad0d
HalfwitGoldfish <Chas2687@gmail.com>
2025-04-08 08:51:20 -0700

    migrations update

--- Migrations/20250408155047_init.cs
+++ Migrations/20250408155047_init.cs
@@ -5,7 +5,7 @@
 namespace plato_backend.Migrations
 {
     /// <inheritdoc />
-    public partial class Init : Migration
+    public partial class init : Migration
     {
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
